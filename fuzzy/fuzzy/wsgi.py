6d2f44dc179e5f806cf45ab45071c963fea90458
Vikash <vikashkisku@gmail.com>
2019-08-04 08:36:13 +0530

    Edited Wsgi.py

--- fuzzy/fuzzy/wsgi.py
+++ fuzzy/fuzzy/wsgi.py
@@ -16,5 +16,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fuzzy.settings')
 
 from whitenoise import WhiteNoise
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+application.add_files(os.path.join(BASE_DIR, 'staticfiles'), prefix='more-files/')
